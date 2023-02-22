be92f0351c5006c1634d0c991763b729fe9db3f6
ABHISHEK KUMAR THAKUR <121752846+abhishekth0203@users.noreply.github.com>
2023-02-22 11:35:55 +0530

    complete with css part

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,84 +1,74 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react';
 
 const Main = () => {
-let [city, setCity] = useState("");
-let [data, setData] = useState([]);
-let [search, setSearch] = useState(false);
+    let [city, setCity] = useState("");
+    let [data, setData] = useState([]);
+    let [search, setSearch] = useState(false);
 
-useEffect(() => {
-    let fetchData = async () => {
-        let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0b868db9e0d6e243fbd1ffe0d4dff998&units=metric`);
-        // let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0b868db9e0d6e243fbd1ffe0d4dff998&units=metric`);
+    useEffect(() => {
+        let fetchData = async () => {
+            let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0b868db9e0d6e243fbd1ffe0d4dff998&units=metric`);
+            let j_data = await res.json();
+            setData([j_data]);
+            console.log([j_data]);
+        }
+        if (search) {
+                fetchData();
+            }
+            setSearch(false);
+        },[search]);
+    
+        let handleSubmit = e => {
+        e.preventDefault();
+        console.log("submitted");
+                setSearch(true);
+            };
         
-        let j_data = await res.json();
-        setData([j_data]);
-        // console.log([j_data]);
-    };
-    if (search) {
-        fetchData();
-    }
-    setSearch(false);
-}, [search]);
-
-
-let handleSubmit = e => {
-    setSearch(true);
-};
-
-  return (
+          return (
     <section className='main-block'>
         <article>
             <form onSubmit={handleSubmit}>
-            <input
-             type="text"
-            placeholder='Search City'
-            onChange={e => {
-                setCity(e.target.value);
-            }}
-            ></input>
-
-            <button>🔍</button>
+                <input 
+                type="text"
+                placeholder='Search Country...'
+                onChange={e => {
+                    setCity(e.target.value);
+                }}
+                ></input>
+                <button>🔍</button>
             </form>
-
 
             <article>
                 {data.map(value => {
-                    return (
-                        <div className='card' key={Math.random()}>
-                        <h1>{value.name}</h1>
-                        <img src={`http://openweathermap.org/img/wn/${value.weather[0].icon}@2x.png`}
-                            width="100px"
-                        />
+                        return (
+                                <div className='card' key={Math.random}>
+                        <h1>{value.name} ( {value.sys.country} )</h1>
+                        <img src={`http://openweathermap.org/img/wn/${value.weather[0].icon}@2x.png`}/>
+                        <br/>
+                        <p className='description'>{value.weather[0].description}</p>
                         <br/>
 
-                        <p>{value.weather[0].description}</p>
-                        <br/>
+                        <p className='temp'> 🌡 : {value.main.temp} <sup>o</sup>C</p>
 
-                        <p>Lon : { value.coord.lon} | Lat : {value.coord.lat}</p>
+                        <p className='hum'>humidity : {value.main.humidity}%</p>
 
-                        <p>Temp : {value.main.temp} <sup>o</sup>C</p>
-
-                        <p>Humidity : {value.main.humidity}</p>
-
-                        <p>Min Temp : {value.main_min} <br/>
-
-                        Max Temp : { value.main.temp_max}
+                        <p className='lon'>Lon : {value.coord.lon} | Lat : {value.coord.lat} </p>
+                        <p className='mtemp'>Min Temp : {value.main.temp_min}<sup>o</sup>C <br/>
+                        Max Temp : {value.main.temp_max}<sup>o</sup>C
                         </p>
 
+                        <p className='wind'>💨 {value.wind.speed}</p>
+                        
+                        <p className='vis'>visibility : {value.visibility} m</p>
+                        <a className='ownerinfo' href='https://github.com/abhishekth0203'>Created By Mr. Abhishek</a>
+                        {/* <p className='ownerinfo'>Created By Mr. Abhishek</p> */}
                         </div>
                     );
                 })}
             </article>
         </article>
     </section>
-  );
+  )
 }
 
-export default Main
-
-
-//   0b868db9e0d6e243fbd1ffe0d4dff998
-
-
-
-//https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=0b868db9e0d6e243fbd1ffe0d4dff998
+export default Main;
